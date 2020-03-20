798ee1439808ac22193482029bfa1a932eb71f69
Bryan Y <xpreyx@gmail.com>
2020-03-20 01:26:25 -0700

    deployed

---
+++ build/precache-manifest.f732c2094c4e9624376e07e73a5f7916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89109826d6004861c4a3b98c930f52f0",
+    "url": "/bchw19-directory/index.html"
+  },
+  {
+    "revision": "383df26d0941cf060d37",
+    "url": "/bchw19-directory/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ffd1aa1bd78ad669ec38",
+    "url": "/bchw19-directory/static/js/2.2738781c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/bchw19-directory/static/js/2.2738781c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383df26d0941cf060d37",
+    "url": "/bchw19-directory/static/js/main.550299ac.chunk.js"
+  },
+  {
+    "revision": "1416283bd26f84221ceb",
+    "url": "/bchw19-directory/static/js/runtime-main.f62a959f.js"
+  }
+]);
